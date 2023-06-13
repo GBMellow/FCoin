4c5168dea63403dcd04c83c5b109757fd1060873
Thiago Pedron <thiagopedron70@gmail.com>
2023-06-13 20:12:59 -0300

    Update validador.py
    Updating to use validator by reference.
    self -> validador.
    Removing save from last transaction and count of transaction

--- entities/eleicao/validador.py
+++ entities/eleicao/validador.py
@@ -7,7 +7,7 @@ from entities.gerenciador.main import Validador
 
 
 class Validar:
-    def validar_transacao(self, remetente_id, valor):
+    def validar_transacao(validador, remetente_id, valor):
         remetente = requests.get(base_url + f"/cliente/{remetente_id}")
         if remetente is None:
             return False
@@ -16,27 +16,24 @@ class Validar:
             return False
 
         horario_atual = datetime.now()
-        print("\n\nself.ultima_transacao", type(self.ultima_transacao))
+        print("\n\nself.ultima_transacao", type(validador.ultima_transacao))
         print("\n\nhorario_atual", horario_atual)
 
-        if type(self.ultima_transacao) == str:
-            self.ultima_transacao = datetime.utcnow().strptime(
-                self.ultima_transacao, "%Y-%m-%d %H:%M:%S.%f"
+        if type(validador.ultima_transacao) == str:
+            validador.ultima_transacao = datetime.utcnow().strptime(
+                validador.ultima_transacao, "%Y-%m-%d %H:%M:%S.%f"
             )
         else:
             pass
-        if self.ultima_transacao is not None and horario_atual <= self.ultima_transacao:
+        if validador.ultima_transacao is not None and horario_atual <= validador.ultima_transacao:
             return False
 
-        if self.contador_transacoes > 1000 and horario_atual.second == 0:
+        if validador.contador_transacoes > 1000 and horario_atual.second == 0:
             return False
-
-        self.ultima_transacao = horario_atual
-        self.contador_transacoes += 1
         return True
 
     def concluir_transacao(self, transacao, validador: Validador):
-        if self.validar_transacao(transacao.remetente, transacao.valor):
+        if self.validar_transacao(validador,transacao.remetente, transacao.valor):
             transacao.status = 1  # Transação concluída com sucesso
         else:
             transacao.status = 2  # Transação não aprovada (erro)
